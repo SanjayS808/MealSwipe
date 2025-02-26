147f2cbc6925b3d084603ccdf3e9842674a742d5
David_Rodriguez <david.rodriguez220443@gmail.com>
2025-02-25 18:26:26 -0600

    Adding CI/CD Changes from Forked Repo.

--- infra/bin/infra.js
+++ infra/bin/infra.js
@@ -3,7 +3,11 @@
 const cdk = require('aws-cdk-lib');
 const { MealSwipeAppService } = require('../lib/infra-stack');
 
-const app = new cdk.App();
+const app = new cdk.App({
+  context: {
+    '@aws-cdk/core:preserveLogicalIds': true
+  }
+});
 new MealSwipeAppService(app, 'MealSwipeAppService', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
@@ -11,7 +15,7 @@ new MealSwipeAppService(app, 'MealSwipeAppService', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
