673dc91cb2aeaaf65f92a8272e1ff204f42e7f9a
David_Rodriguez <david.rodriguez220443@gmail.com>
2025-03-19 10:38:02 -0500

    Added basic API functionality for database. Work on frontend in progress.

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ const app = express();
 const API_URI = "https://mealswipe-flask-service.75ct69eg04jk6.us-west-2.cs.amazonlightsail.com/v1/places:searchNearby";
 
 app.use(cors());  // Enable CORS
+app.use(express.json()); // This enables JSON body parsing
+app.use(express.urlencoded({ extended: true })); // Support for URL-encoded data
 
 app.get("/api/serve/get-all-restaurants", (req, res) => {
     const data = {
@@ -63,14 +65,242 @@ app.get("/api/serve/get-all-restaurants", (req, res) => {
     
 });
 
-app.get("/api/serve/add-liked-restaurant", async (req, res) => {
+// Get userid via username
+app.get("/api/serve/get-userid-with-uname", async (req, res) => {
+    if(req.query.uname === undefined) {
+        console.error('Could not fetch undefined user.');
+        res.status(400).json({error: 'Could not fetch undefined username.'});
+        return;
+    }
+    const query = `SELECT userid FROM Users WHERE name='${req.query.uname}';`
     try {
-        const result = await pool.query('SELECT * FROM Users;');
+        const result = await pool.query(query);
         res.json(result.rows);
     } catch (err) {
         console.error('Error fetching users:', err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+});
+
+// Get user info via ID
+app.get("/api/serve/get-user-with-id", async (req, res) => {
+    if(req.query.uid === undefined) {
+        console.error('Could not fetch undefined user.');
+        res.status(400).json({error: 'Could not fetch undefined user.'});
+        return;
+    }
+    const query = `SELECT * FROM Users WHERE userid='${req.query.uid}';`
+    try {
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post("/api/serve/add-user", async (req, res) => {
+    // Check and insert all information needed for creating new user.
+    const hasNullValue = (array) => array.some(element => element === undefined);
+    const uinfo = [req.body.uid, req.body.uname, req.body.ubio, req.body.nswipes];
+    if(hasNullValue(uinfo)) {
+        console.error('Could not create user. User information is missing.');
+        res.status(400).json({error: 'Could not create user. User information is missing.'});
+        return;
+    }
+
+    const add_query = `INSERT INTO Users 
+    VALUES (${req.body.uid}, '${req.body.uname}', '${req.body.ubio}', ${req.body.nswipes});`;
+
+    const get_query = `SELECT * FROM Users WHERE name='${req.body.uname}'`;
+
+    try {
+        // Check if user already exists.
+        const get_result = await pool.query(get_query);
+        if(get_result.rows.length != 0) {
+            throw Error(`Username ${req.body.uname} already exists.`);
+        }
+
+        // If not add, user.
+        const add_result = await pool.query(add_query);
+        if(add_result.rowCount != 1) {
+            throw Error(`POST function modified unintended columns.`);
+        }
+
+        res.status(200).json("User succesfully added.")
+
+    } catch(err) {
+        console.error('Error creating new user: ', err);
+        res.status(500).json({error: 'Internal Server Error'})
+    } 
+});
+
+// Gets restaurant id via the restaurant's name.
+app.get("/api/serve/get-rid-with-rname", async (req, res) => {
+    if(req.query.rname === undefined) {
+        console.error('Could not fetch undefined restuarant.');
+        res.status(400).json({error: 'Could not fetch undefined restuarant.'});
+        return;
+    }
+    const query = `SELECT name FROM restaurants WHERE name='${req.query.rname}';`
+    try {
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+// Gets restaurant info via the restaurant's id.
+app.get("/api/serve/get-rinfo-with-rid", async (req, res) => {
+    if(req.query.rid === undefined) {
+        console.error('Could not fetch undefined restuarant.');
+        res.status(400).json({error: 'Could not fetch undefined restuarant.'});
+        return;
+    }
+    const query = `SELECT * FROM restaurants WHERE placeid='${req.query.rid}';`
+    try {
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+})
+
+app.post("/api/serve/add-restaurant", async (req, res) => {
+    // Check and insert all information needed for creating new user.
+    const hasNullValue = (array) => array.some(element => element === undefined);
+    const uinfo = [req.body.rid, req.body.rname, req.body.price, req.body.rating,
+        req.body.weburl, req.body.gmapurl];
+    if(hasNullValue(uinfo)) {
+        console.error('Could not create user. User information is missing.');
+        res.status(400).json({error: 'Could not create user. User information is missing.'});
+        return;
+    }
+
+    const add_query = `INSERT INTO Restaurants 
+    VALUES (${req.body.rid}, '${req.body.rname}', ${req.body.price}, ${req.body.rating},
+    '${req.body.weburl}', '${req.body.gmapurl}');`;
+
+    const get_query = `SELECT * FROM Restaurants WHERE name='${req.body.rname}'`;
+
+    try {
+        // Check if user already exists.
+        const get_result = await pool.query(get_query);
+        if(get_result.rows.length != 0) {
+            throw Error(`Username ${req.body.uname} already exists.`);
+        }
+
+        // If not add, user.
+        const add_result = await pool.query(add_query);
+        if(add_result.rowCount != 1) {
+            throw Error(`POST function modified unintended columns.`);
+        }
+
+        res.status(200).json("User succesfully added.")
+
+    } catch(err) {
+        console.error('Error creating new user: ', err);
+        res.status(500).json({error: 'Internal Server Error'})
+    } 
+})
+
+app.get("/api/serve/get-user-trashed-restaurant", async (req, res) => {
+    if(req.query.uid === undefined) {
+        console.error('Could not fetch undefined user.');
+        res.status(400).json({error: 'Could not fetch undefined user.'});
+        return;
+    }
+    const query = `SELECT * FROM trashed_swipes WHERE userid='${req.query.uid}';`
+    try {
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error fetching trashed restaurants:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// User provides userid and restaurant name. We check if entities
+// With these values exist, we add relationship in table.
+app.post("/api/serve/add-user-trashed-resstaurant", async (req, res) => {
+    // Check and insert all information needed for creating new user.
+    const hasNullValue = (array) => array.some(element => element === undefined);
+    const uinfo = [req.body.uid, req.body.rname];
+    if(hasNullValue(uinfo)) {
+        console.error('Could not create user. User information is missing.');
+        res.status(400).json({error: 'Could not create user. User information is missing.'});
+        return;
+    }
+
+    const add_query = `INSERT INTO trahsed_swipes 
+    VALUES ((SELECT userid FROM Users WHERE userid=${uid}), 
+    (SELECT placeid FROM Restaurants WHERE name=${rname}));`;
+
+    try {
+        // If not add, user.
+        const add_result = await pool.query(add_query);
+        if(add_result.rowCount != 1) {
+            throw Error(`POST function modified unintended columns.`);
+        }
+
+        res.status(200).json("User succesfully added.")
+
+    } catch(err) {
+        // One of the more common errors will be that either the restaurant or user entity,
+        // does not exist.
+        console.error('Error creating new user: ', err);
+        res.status(500).json({error: 'Internal Server Error'})
+    } 
+});
+
+app.get("/api/serve/get-user-liked-restaurants", async (req, res) => {
+    if(req.query.uid === undefined) {
+        console.error('Could not fetch undefined user.');
+        res.status(400).json({error: 'Could not fetch undefined user.'});
+        return;
+    }
+    const query = `SELECT * FROM liked_swipes WHERE userid='${req.query.uid}';`
+    try {
+        const result = await pool.query(query);
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Error fetching users:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post("/api/serve/add-user-liked-restaurant", async (req, res) => {
+     // Check and insert all information needed for creating new user.
+     const hasNullValue = (array) => array.some(element => element === undefined);
+     const uinfo = [req.body.uid, req.body.rname];
+     if(hasNullValue(uinfo)) {
+         console.error('Could not create user. User information is missing.');
+         res.status(400).json({error: 'Could not create user. User information is missing.'});
+         return;
+     }
+ 
+     const add_query = `INSERT INTO liked_swipes 
+     VALUES ((SELECT userid FROM Users WHERE userid=${uid}), 
+     (SELECT placeid FROM Restaurants WHERE name=${rname}));`;
+ 
+     try {
+         // If not add, user.
+         const add_result = await pool.query(add_query);
+         if(add_result.rowCount != 1) {
+             throw Error(`POST function modified unintended columns.`);
+         }
+ 
+         res.status(200).json("User succesfully added.")
+ 
+     } catch(err) {
+         // One of the more common errors will be that either the restaurant or user entity,
+         // does not exist.
+         console.error('Error creating new user: ', err);
+         res.status(500).json({error: 'Internal Server Error'})
+     } 
 });
 
 
