40d83939079558829117976780091c625f159ed3
SanjayS808 <sanjaysugumar20@gmail.com>
2025-04-12 23:34:29 -0500

    made loader smaller

--- frontend_web/src/components/Loader.js
+++ frontend_web/src/components/Loader.js
@@ -6,8 +6,8 @@ const Loader = () => {
         
     <div class="mainPizza">
     <svg
-        width="168"
-        height="158"
+        width="5em"
+        height="5em"
         viewBox="0 0 168 158"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
